dca234993c21cc9ec2cfc1c928c5e53cb1bb3204
ZelongY <ZelongY@pm.me>
2023-08-29 11:15:40 +0800

    服务端与客户端更新

---
+++ IClient/changecolor_window.cpp
@@ -0,0 +1,74 @@
+#include "changecolor_window.h"
+#include "ui_changecolor_window.h"
+
+ChangeColor_Window::ChangeColor_Window(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::ChangeColor_Window)
+{
+    ui->setupUi(this);
+    setFixedSize(width(), height());
+    //ui->logo->setSizeIncrement(50,50);
+    //ui->logo->setIcon(QIcon(":/new/prefix1/img/logo.jpg"));
+    //ui->logo->setIconSize(QSize(100,100));
+}
+
+ChangeColor_Window::~ChangeColor_Window()
+{
+    delete ui;
+}
+
+void ChangeColor_Window::on_icebreak_clicked()
+{
+    ui->widget->setStyleSheet("background-color:"+ChangeColor_Window::icebreak+";"+ "border:none;");
+    emit sig_color(ChangeColor_Window::icebreak);
+}
+
+
+void ChangeColor_Window::on_macaroon_clicked()
+{
+    ui->widget->setStyleSheet("background-color:"+ChangeColor_Window::macaroon+";"+ "border:none;");
+    emit sig_color(ChangeColor_Window::macaroon);
+}
+
+void ChangeColor_Window::on_morandi_clicked()
+{
+    ui->widget->setStyleSheet("background-color:"+ChangeColor_Window::morandi+";"+ "border:none;");
+    emit sig_color(ChangeColor_Window::morandi);
+}
+
+void ChangeColor_Window::on_red_clicked()
+{
+    ui->widget->setStyleSheet("background-color:"+ChangeColor_Window::red+";"+ "border:none;");
+    emit sig_color(ChangeColor_Window::red);
+}
+
+void ChangeColor_Window::on_yellow_clicked()
+{
+    ui->widget->setStyleSheet("background-color:"+ChangeColor_Window::yellow+";"+ "border:none;");
+    emit sig_color(ChangeColor_Window::yellow);
+}
+
+void ChangeColor_Window::on_green_clicked()
+{
+    ui->widget->setStyleSheet("background-color:"+ChangeColor_Window::green+";"+ "border:none;");
+    emit sig_color(ChangeColor_Window::green);
+}
+
+void ChangeColor_Window::on_pink_clicked()
+{
+    ui->widget->setStyleSheet("background-color:"+ChangeColor_Window::pink+";"+ "border:none;");
+    emit sig_color(ChangeColor_Window::pink);
+}
+
+void ChangeColor_Window::on_orange_clicked()
+{
+    ui->widget->setStyleSheet("background-color:"+ChangeColor_Window::orange+";"+ "border:none;");
+    emit sig_color(ChangeColor_Window::orange);
+}
+
+void ChangeColor_Window::on_purple_clicked()
+{
+    ui->widget->setStyleSheet("background-color:"+ChangeColor_Window::purple+";"+ "border:none;");
+    emit sig_color(ChangeColor_Window::purple);
+}
+
